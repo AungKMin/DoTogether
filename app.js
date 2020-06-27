7c410fe6ced1b68067a71168171a5e3d654d414e
AungKMin <aungkhantmin2014@gmail.com>
2020-06-26 20:35:29 -0400

    changed 3000 to env

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.use('/activities', activityRoutes)
 app.use('/activities/:id/comments', commentRoutes)
 app.use(indexRoutes)
 
-app.listen(3000, process.env.IP, function() { 
+app.listen(process.env.PORT, process.env.IP, function() { 
 	console.log('Server Started')
 })
